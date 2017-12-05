bb329be8a2f9f29e3523f97002dca91b4e23bd1b
sergey.koumirov <sergey.koumirov@castle.co>
2017-12-05 12:25:47 +0400

    migrate

--- manage.py
+++ manage.py
@@ -2,4 +2,4 @@
 from migrate.versioning.shell import main
 
 if __name__ == '__main__':
-    main(url='mysql://aes:aes@localhost/aes?charset=utf8', repository='./migrations', debug='False')
+    main(url='mysql+pymysql://aes:aes@localhost/aes?charset=utf8', repository='./migrations', debug='False')
