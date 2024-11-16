2f8604d1b243aa962593ab8869faedc10588a318
merkantiloid <merkantiloid@yandex.ru>
2024-11-16 13:05:58 +0300

    some changes, without swagger and esi modification

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
+from app import views
+
 from flask import Flask, g, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
-from .filters import fldate
+# from app.filters import fldate # from app.filters import fldate - GPt chat замена относ. пути на абсолют.
 
-from esipy import App
+from esipy.app import EsiApp
 from esipy import EsiClient
 from esipy import EsiSecurity
 
-from datetime import datetime
-import os
+from datetime import datetime, timezone
+from .models import User
+# import os
 
 mainApp = Flask(__name__)
 mainApp.config.from_object('config')
@@ -25,6 +28,10 @@ if not mainApp.debug:
     mainApp.logger.addHandler(handler)
 
 
+def fldate(value):
+    return datetime.fromtimestamp(value, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')
+
+
 mainApp.jinja_env.filters['fldate'] = fldate
 
 db = SQLAlchemy(mainApp)
@@ -33,18 +40,17 @@ lm = LoginManager()
 lm.init_app(mainApp)
 
 
-
 # def _hook(url):
 #     return 'file://'+os.getcwd()+'/swagger.t.json'
 # esiapp = App.load(mainApp.config.get('ESI_SWAGGER_JSON',''), url_load_hook=_hook)
 
-esiapp = App.load(mainApp.config.get('ESI_SWAGGER_JSON',''))
+esiapp = EsiApp(meta_url=mainApp.config.get('ESI_SWAGGER_JSON', ''))
 esiapp.prepare()
 esisecurity = EsiSecurity(
     app=esiapp,
-    redirect_uri=mainApp.config.get('ESI_CALLBACK_URL',''),
-    client_id=mainApp.config.get('ESI_CLIENT_ID',''),
-    secret_key=mainApp.config.get('ESI_SECRET',''),
+    redirect_uri=mainApp.config.get('ESI_CALLBACK_URL', ''),
+    client_id=mainApp.config.get('ESI_CLIENT_ID', ''),
+    secret_key=mainApp.config.get('ESI_SECRET', ''),
 )
 
 esiclient = EsiClient(
@@ -54,31 +60,36 @@ esiclient = EsiClient(
 )
 
 
-from .models import User
-
-
 @lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @lm.unauthorized_handler
 def unauthorized_callback():
     return redirect('/login')
 
+
 @mainApp.after_request
 def save_user_action(response):
     if g.user.is_authenticated:
-      db.session.add( models.UserAction(user_id=g.user.id, path=str(request.url_rule), created_at=datetime.now().isoformat()) )
-      db.session.commit()
+        db.session.add(models.UserAction(
+            user_id=g.user.id,
+            path=str(request.url_rule),
+            created_at=datetime.now().isoformat())
+        )
+        db.session.commit()
     else:
-      db.session.add( models.UserAction(user_id=-1, path=str(request.url_rule), created_at=datetime.now().isoformat()) )
-      db.session.commit()
+        db.session.add(models.UserAction(
+            user_id=-1,
+            path=str(request.url_rule),
+            created_at=datetime.now().isoformat())
+        )
+        db.session.commit()
 
     return response
+
 
 @mainApp.before_request
 def before_request():
     g.user = current_user
-
-from app import views
